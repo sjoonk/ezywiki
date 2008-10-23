8cc0a2da9ec69affba66c137d8922fcd7e5e886e
sjoonk <sjoonk@f8962f1e-9153-11dd-8180-2593c701d5d0>
2008-10-23 03:13:19 +0000

    Resolving html entities mismatching probiem

--- trunk/public/javascripts/application.js
+++ trunk/public/javascripts/application.js
@@ -1,6 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+// Unescape HTML entities
+String.prototype.unescapeHtmlEntities = function () {
+	try{
+		var temp = document.createElement('textarea');
+    temp.innerHTML = this;
+    return temp.value;
+	}catch(e){
+		return this;
+	}
+}
+
 $(document).ready(function() {
 	// $.ajaxSetup({ data: { authenticity_token: encodeURIComponent(AUTH_TOKEN) }});
 	// $('.rounded').parent().css('background-color', '#fff').end().corner(); // http://www.methvin.com/jquery/jq-corner.html
